fd9b2f0db56773640f5df59396c6aa5c28243663
Viru Kanjilal <v.kanjilal@gmail.com>
2021-12-05 12:39:25 -0500

    tree pattern

---
+++ patterns/01_level_order_tree_traversal/level_order_traversal.py
@@ -0,0 +1,59 @@
+# 102. Binary Tree Level Order Traversal
+# https://leetcode.com/problems/binary-tree-level-order-traversal/
+#
+# This pattern performs a level order traversal of a binary tree. At
+# each level it stores the nodes of the next level in the queue. Before
+# iterating over the nodes in the next level, it keeps a record of the
+# length of the queue so that it knows when the level is done.
+
+import collections
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> [[int]]:
+
+        # Handle the case with None root
+        if root is None:
+            return None
+        
+        # This is the output list
+        output = []
+
+        # Create a double ended queue.
+        # The contains a list object with root node. The queue is list of tree nodes.
+        queue = collections.deque([root])
+
+        while len(queue) > 0:
+            # The queue at this point contains all the nodes at a given level.
+
+            # length of the queue at each level
+            levelLen = len(queue)
+
+            # store all the nodes of the next level of the tree in this list
+            level = []
+
+            # Iterate over all the elements in this level
+            for i in range(0, levelLen):
+
+                # using popleft() since we are using a deque
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+                # Store the node values of this level in this list
+                level.append(node.val)
+
+            # Store the node
+            output.append(level)
+
+        # The output contains the nodes in each level as a list object
+        return output
---
+++ patterns/01_level_order_tree_traversal/problems/0103_Binary_Tree_Zigzag_Level_Order_Traversal.py
@@ -0,0 +1,66 @@
+# 103. Binary Tree Zigzag Level Order Traversal
+# https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+#
+# This uses the level order tree pattern.
+
+import collections
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> [[int]]:
+
+        # Handle the case with None root
+        if root is None:
+            return None
+
+        # This is the output list
+        output = []
+
+        # Create a double ended queue.
+        # The contains a list object with root node. The queue is list of tree nodes.
+        queue = collections.deque([root])
+
+        # This is a boolean toogle variable to keep track of whether to reverse a
+        # level or not. The value is alternated for every level.
+        is_reverse = False
+
+        while len(queue) > 0:
+            # The queue at this point contains all the nodes at a given level.
+
+            # length of the queue at each level
+            levelLen = len(queue)
+
+            # store all the nodes of the next level of the tree in this list
+            level = []
+
+            # Iterate over all the elements in this level
+            for i in range(0, levelLen):
+
+                # using popleft() since we are using a deque
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+                # Store the node values of this level in this list
+                level.append(node.val)
+
+            # Store the node
+            if is_reverse:
+                output.append(reversed(level))
+            else:
+                output.append(level)
+
+            # toggle the value of the is_reverse variable
+            is_reverse = not is_reverse
+
+        # The output contains the nodes in each level as a list object
+        return output
